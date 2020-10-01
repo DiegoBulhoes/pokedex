5c38765e4330ae26b55ad36ec4f634700d845539
Vinicius Espindola <vinnyaoe@gmail.com>
2020-10-01 19:07:42 -0400

    configure router

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,10 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <button @click="seila"></button>
-  <div>{{ teste }}</div>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-import {ref}  from 'vue';
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  setup(){
-    let teste = ref("teste");
-      function seila(){
-        alert("seila");
-      }
-    return {
-      teste,
-      seila
-    }
-
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
