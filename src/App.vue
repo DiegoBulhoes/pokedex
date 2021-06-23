ecc8964af235cc4568bd681d4cfac39586bdc791
fernando <kendyyahiro@Hotmail.com>
2021-06-23 17:17:41 -0400

    Footer - Inserido um rodapé para demarcar e melhorar visualmente além das buscas de Pokémon

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,19 @@
     <v-main>
       <router-view></router-view>
     </v-main>
+    <Footer></Footer>
   </v-app>
 </template>
 
 <script>
 import AppBar from '@/components/AppBar.vue';
+import Footer from '@/components/Footer.vue';
 
 export default {
   name: 'App',
   components: {
     AppBar,
+    Footer,
   },
 };
 </script>
